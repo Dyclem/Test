614c19e30b9c73b4e35ed5bcb1f98122a66be0c6
Dyclem <145921134+Dyclem@users.noreply.github.com>
2023-09-24 17:37:30 +0100

    Update Age_Verif.js

--- Age_Verif.js
+++ Age_Verif.js
@@ -1,10 +1,7 @@
-function verifyAge(isOver18) {
-    var overlay = document.getElementById('ageVerificationOverlay');
-    if (isOver18) {
-        // If the user is over 18, hide the overlay
-        overlay.style.display = 'none';
-    } else {
-        // If the user is under 18, redirect to google.com
+window.onload = function() {
+    alert("Welcome to Quirky Apple!\nYou must be at least 18 years old to enter this site.\n\nBy proceeding, you certify that you are of legal age and understand the laws regarding age verification in your country.");
+    var userConfirmation = confirm("Do you wish to proceed?");
+    if (!userConfirmation) {
         window.location.href = 'http://www.google.com';
     }
-}
+};
